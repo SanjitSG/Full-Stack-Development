8d331c5f11024d6aaa315395859743ac83e3f145
Sanjit Gawade <45919581+SanjitSG@users.noreply.github.com>
2023-06-11 01:18:17 +0530

    JavaScript Exercise

---
+++ Exercise/Javascript/script.js
@@ -0,0 +1,40 @@
+// 1. Write a program to print factorial of any number
+// e.g. : Factorial of 5! => 5 * 4 * 3 * 2 * 1 = 120
+
+// let number = +prompt('Enter a positive number to find Factorial');
+// let fact = 1,
+//   temp = number; //for printing purpose.
+
+// while (number >= 1) {
+//   // 4 // 3 // 2 // 1
+//   fact = fact * number; // 1*4 // 4*3 = 12 // 12*2=24 // 24 *1
+//   number--; // 3 // 2 // 1
+// }
+
+// console.log(`Factorial of ${temp} is ${fact}`); //Factorial of 4 is 24
+
+/*---------------------------------------------------------------------------- */
+
+// 2. write a program to print fibonacci series up to 10 numbers.
+// Fibonacci series => '0 1' 1 2 3 5 8 13 21 34
+// default 0 1 = 1
+//           1 + 1 = 2
+//               1 + 2 = 3
+//                   2 + 3 = 5
+//                       3 + 5 = 8
+//                           5 + 8 = 13 ...
+
+// let terms = +prompt('Enter number of terms');
+// let digitOne = 0,
+//   digitSecond = 1,
+//   fib;
+// 5;
+// console.log('Fibonacci Series: ');
+// for (let i = 1; i <= terms; i++) {
+//   console.log(digitOne);
+//   fib = digitOne + digitSecond;
+//   digitOne = digitSecond;
+//   digitSecond = fib;
+// }
+
+/*---------------------------------------------------------------------------- */

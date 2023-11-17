02136e99e6c0e28d7f95144fedea0a3a87ee50df
Sanjit Gawade <45919581+SanjitSG@users.noreply.github.com>
2023-11-17 23:39:57 +0530

    weather api

--- Exercise/Javascript/1-weather app/script.js
+++ Exercise/Javascript/1-weather app/script.js
@@ -1,17 +1,69 @@
-let btn = document.querySelector('#btn');
-const APIKey = '287d43ef80fa0789a7c341e02d27c727';
+const container = document.querySelector('.container');
+const search = document.querySelector('.search-box button');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-details');
+const error404 = document.querySelector('.not-found');
 
-btn.addEventListener('click', (e) => {
-  e.preventDefault();
+search.addEventListener('click', () => {
+  const APIKey = '287d43ef80fa0789a7c341e02d27c727';
+  let city = document.querySelector('.search-box input').value;
 
-  let city = document.querySelector('#city');
-  let cityName = city.value;
-  console.log(cityName);
-  weatherApi(cityName);
+  if (city == '') return;
+
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
+    .then((response) => response.json())
+    .then((json) => {
+      if (json.cod == '404') {
+        container.style.height = '400px';
+        weatherBox.classList.remove('active');
+        weatherDetails.classList.remove('active');
+        error404.classList.add('active');
+        return;
+      }
+
+      container.style.height = '555px';
+      weatherBox.classList.add('active');
+      weatherDetails.classList.add('active');
+      error404.classList.remove('active');
+
+      const image = document.querySelector('.weather-box img');
+      const temperature = document.querySelector('.weather-box .temperature');
+      const description = document.querySelector('.weather-box .description');
+      const humidity = document.querySelector('.weather-details .humidity span');
+      const wind = document.querySelector('.weather-details .wind span');
+
+      switch (json.weather[0].main) {
+        case 'Clear':
+          image.src = './images/clear.png';
+          break;
+
+        case 'Rain':
+          image.src = './images/rain.png';
+          break;
+
+        case 'Snow':
+          image.src = './images/snow.png';
+          break;
+
+        case 'Clouds':
+          image.src = './images/cloud.png';
+          break;
+
+        case 'Mist':
+          image.src = './images/mist.png';
+          break;
+
+        case 'Haze':
+          image.src = './images/mist.png';
+          break;
+
+        default:
+          image.src = './images/cloud.png';
+      }
+
+      temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
+      description.innerHTML = `${json.weather[0].description}`;
+      humidity.innerHTML = `${json.main.humidity}%`;
+      wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
+    });
 });
-
-let weatherApi = async (cityName) => {
-  let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKey}`);
-  let data = await response.json();
-  console.log(data);
-};

159380a6804d35c03c796bd616d00b68a499016c
Sanjit Gawade <45919581+SanjitSG@users.noreply.github.com>
2023-12-07 23:56:29 +0530

    wrapper-card

---
+++ React/demoapp/src/Components/Wrapper-component/Card.js
@@ -0,0 +1,10 @@
+let Card = () => {
+  return (
+    <div>
+      <h3>Card Component</h3>
+      <h2>Title:</h2>
+    </div>
+  );
+};
+
+export default Card;
--- React/demoapp/src/Components/Wrapper-component/ProductDetails.js
+++ React/demoapp/src/Components/Wrapper-component/ProductDetails.js
@@ -1,11 +1,16 @@
 let ProductDetails = ({ otherProps }) => {
   let {
+    id,
     category,
     price,
     rating: { rate },
   } = otherProps;
   return (
     <div>
+      <h3>
+        <u>Product Details:</u>
+      </h3>
+      <p>Product ID: {id}</p>
       <p>Category: {category}</p>
       <p>Price: &#8377; {price}</p>
       <p>Rating: {rate}</p>

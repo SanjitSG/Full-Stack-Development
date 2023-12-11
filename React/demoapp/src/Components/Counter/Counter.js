1deb346053f2a668d9ff3443087c385a367516ff
Sanjit Gawade <45919581+SanjitSG@users.noreply.github.com>
2023-12-11 23:35:33 +0530

    increment decrement fn using useState

--- React/demoapp/src/Components/Counter/Counter.js
+++ React/demoapp/src/Components/Counter/Counter.js
@@ -1,9 +1,39 @@
+import { useState } from "react";
+
 let Counter = () => {
-  let initValue = 0;
+  // let initValue = 0;
+  let [initValue, SetInitValue] = useState(0);
+
+  let increment = () => {
+    // console.log("increment");
+    SetInitValue((initValue) => (initValue = initValue + 1));
+    console.log(initValue);
+  };
+
+  let decrement = () => {
+    // console.log("decrement");
+    SetInitValue((initValue) => (initValue = initValue - 1));
+    console.log(initValue);
+  };
   return (
     <div>
       <h1>Counter Component</h1>
       <h3>{initValue}</h3>
+      <hr />
+      <button
+        onClick={() => {
+          increment();
+        }}
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => {
+          decrement();
+        }}
+      >
+        Decrement
+      </button>
     </div>
   );
 };

0284c4a8074df6df2d5e69e22af0efcc5d4279cd
Sanjit Gawade <45919581+SanjitSG@users.noreply.github.com>
2023-12-13 16:27:51 +0530

    cart using useState

--- React/demoapp/src/Components/Counter/Products.js
+++ React/demoapp/src/Components/Counter/Products.js
@@ -1,22 +1,63 @@
 import { useState } from "react";
 import data from "./data";
 
-let addToCart = () => {};
 let Products = () => {
-  let card = {};
+  let [count, setCount] = useState(0);
+  let [cart, setCart] = useState([]);
+
+  // css
+  let card = {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-evenly",
+  };
+
+  let child = {
+    border: "2px solid red",
+    minHeight: "400px",
+    width: "400px",
+    padding: "20px",
+    margin: "10px",
+  };
+  let addToCart = (product) => {
+    // console.log(product);
+
+    setCart((cart) => [...cart, product]);
+    setCount((count) => (count = count + 1));
+  };
+  let removeFromCart = () => {};
   return (
     <div>
       <h1>Products Component</h1>
-      {data.map((product) => (
-        <div key={product.id}>
-          <h2>{product.title}</h2>
-          <p>{product.description}</p>
-          <img src={product.image} alt={product.title} style={{ height: "200px", width: "200px" }} />
-          <p>{product.price}</p>
-          <button onClick={addToCart()}>Add to cart</button>
-          <hr />
-        </div>
-      ))}
+      {/* dynamically change the values */}
+      <h3>No of products in card: {count}</h3>
+      <h3>The Products in the cart: {cart.join(", ")}</h3>
+      <hr />
+      <div style={card}>
+        {data.map((product) => (
+          <div key={product.id} style={child}>
+            <h2>{product.title}</h2>
+            <p>{product.description}</p>
+            <img src={product.image} alt={product.title} style={{ height: "100px", width: "100px" }} />
+            <p>&#8377; {product.price}</p>
+
+            <button
+              onClick={() => {
+                addToCart(product.title);
+              }}
+            >
+              Add to cart
+            </button>
+            <button
+              onClick={() => {
+                removeFromCart();
+              }}
+            >
+              Remove
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- React/demoapp/src/Components/Wrapper-component/ProductCard.js
+++ React/demoapp/src/Components/Wrapper-component/ProductCard.js
@@ -5,7 +5,6 @@ let ProductCard = ({ product }) => {
   let { title, description, ...otherProps } = product;
   return (
     <Card title={title}>
-      {/* <h1>Title: {title}</h1> */}
       <p>Description: {description}</p>
       <ProductDetails otherProps={otherProps} />
       <hr />

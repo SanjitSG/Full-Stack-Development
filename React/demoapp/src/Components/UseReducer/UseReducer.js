ae9a25b559933f21bad27b255a04a1d51ec0807c
Sanjit Gawade <45919581+SanjitSG@users.noreply.github.com>
2023-12-15 00:12:53 +0530

    useEffect, useRef

--- React/demoapp/src/Components/UseReducer/UseReducer.js
+++ React/demoapp/src/Components/UseReducer/UseReducer.js
@@ -1,4 +1,4 @@
-import { useReducer } from "react";
+import { useReducer, useEffect, useRef } from "react";
 
 let UseReducer = () => {
   let initialState = {
@@ -7,8 +7,37 @@ let UseReducer = () => {
   };
 
   let [state, dispatch] = useReducer(reducer, initialState);
+  let idRef = useRef(0);
 
-  function reducer() {}
+  useEffect(() => {
+    if (!state.isRunning) {
+      return;
+    }
+
+    idRef.current = setInterval(() => dispatch({ type: "tick" }), 1000);
+    return () => {
+      clearInterval(idRef.current);
+      idRef.current = 0;
+    };
+  }, [state.isRunning]);
+
+  function reducer(state, action) {
+    switch (action.type) {
+      case "start":
+        console.log("start");
+        return { ...state, isRunning: true };
+      case "stop":
+        console.log("stop");
+        return { ...state, isRunning: false };
+      case "reset":
+        console.log("reset");
+        return { ...state, time: 0 };
+      case "tick":
+        return { ...state, time: state.time + 1 };
+      default:
+        throw new Error();
+    }
+  }
   return (
     <div>
       <p>{state.time}s</p>

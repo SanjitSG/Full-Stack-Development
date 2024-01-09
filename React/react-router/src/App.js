238f37c86da5a1956e875fd4c846a79946382cd5
Sanjit Gawade <45919581+SanjitSG@users.noreply.github.com>
2024-01-09 21:03:08 +0530

    react-router

--- React/react-router/src/App.js
+++ React/react-router/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Home from "./Components/Home";
+import Projects from "./Components/Projects";
+import Error from "./Components/Error";
+import Navbar from "./Components/Navbar";
+
+//react router dom
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Products from "./Components/Products";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/Projects" element={<Projects />} />
+          <Route path="/Contacts" element={<Contact />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ React/react-router/src/Components/Projects.js
@@ -0,0 +1,9 @@
+const Projects = () => {
+  return (
+    <div>
+      <h1>Projects component</h1>
+    </div>
+  );
+};
+
+export default Projects;

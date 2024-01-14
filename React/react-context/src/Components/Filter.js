cc1054285396a305f3dc8e93085fb4805a3ba3c3
Sanjit Gawade <45919581+SanjitSG@users.noreply.github.com>
2024-01-14 22:10:11 +0530

    filter

--- React/react-context/src/Components/Filter.js
+++ React/react-context/src/Components/Filter.js
@@ -1,7 +1,12 @@
+import { Form } from "react-bootstrap";
 const Filter = () => {
   return (
-    <div>
-      <h2>Filter Component</h2>
+    <div className="filters">
+      <span className="title">Filter Section</span>
+      <span>
+        <Form.Check inline label="A-Z" name="group" type="radio" />
+        <Form.Check inline label="Z-A" name="group" type="radio" />
+      </span>
     </div>
   );
 };
